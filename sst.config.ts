28b7241198adaff36d05b9bfe1158eee8ea90304
ironheart122 <18900204+ironheart122@users.noreply.github.com>
2025-02-13 12:46:27 +0530

    chore: set up aws provider config in sst config

--- sst.config.ts
+++ sst.config.ts
@@ -2,11 +2,23 @@
 
 export default $config({
   app(input) {
+    const isProduction = input?.stage === "production";
+
     return {
       name: "react-router-7-app",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
+      removal: isProduction ? "retain" : "remove",
+      protect: isProduction,
       home: "aws",
+      providers: {
+        aws: {
+          profile: process.env.GITHUB_ACTIONS
+            ? undefined
+            : isProduction
+            ? "kyroslabs-production"
+            : "kyroslabs-development",
+          region: "eu-central-1",
+        },
+      },
     };
   },
   async run() {
